07f7b8dd8e0836a4030d75003af51b5351fcb43f
Yarwhal <shihaozh@hotmail.com>
2023-03-05 16:05:22 -0500

    Second Push

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+bool keyGet = false;
 public float movementSpeed = 1f;
 public Vector2 movement;           //Movement Axis
 public Rigidbody2D rigidbody;      //Player Rigidbody Component
@@ -24,9 +25,17 @@ private void FixedUpdate()
     rigidbody.MovePosition(rigidbody.position + movement * movementSpeed * Time.fixedDeltaTime);
 }
 
-void OnCollisionEnter2D(Collision2D col)
-{
-	Debug.Log("OnCollisionEnter2D");
-}
+private void OnCollisionEnter2D(Collision2D collision){
+   Debug.Log("ah");
+     if(collision.gameObject.tag == "Key")
+     {
+        Destroy(collision.collider.gameObject);
+        keyGet = true;
+     }
+     if(collision.gameObject.tag == "Door" && keyGet == true)
+     {
+        Object.Destroy(collision.gameObject);
+     }
+ }
 
 }
